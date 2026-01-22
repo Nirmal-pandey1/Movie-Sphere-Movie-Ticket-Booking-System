faf1a354ae0421be6023b44143605c1eb243cc4c
Nirmal Pandey <nirmalpandey680@gmail.com>
2026-01-22 20:08:11 +0530

    setup the fronted project

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Slider } from '@mantine/core';
+import { useHover } from '@mantine/hooks';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+ const { hovered, ref } = useHover();
+  return (
+    <>
+      <h1 className="text-amber-100">hello </h1>
+       <Slider
+      defaultValue={40}
+      min={10}
+      max={90}
+      ref={ref}
+      label={null}
+      styles={{
+        thumb: {
+          transition: 'opacity 150ms ease',
+          opacity: hovered ? 1 : 0,
+        },
+      }}
+    />
+    </>
+  )
+}
+
+export default App
